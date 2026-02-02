bd3d8b653bb735bcc2f6c0eb2e14d63bdfee3f4e
zacharykrobinson1-dev <zacharykrobinson1@gmail.com>
2026-02-02 16:40:35 +0000

    final

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
   eleventyConfig.addPassthroughCopy("name.js");
